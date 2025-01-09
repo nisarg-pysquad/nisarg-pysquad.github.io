3bbb3ebd0193974b4c1243b9112d6c4016b8549b
Nisarg Patel <nisarg@pysquad.com>
2025-01-09 12:36:05 +0530

    [IMP] Implimented code

---
+++ assets/js/Data.js
@@ -0,0 +1,178 @@
+export const UserData = [
+  { image: "assets/images/PYS_Icon.svg" },
+  { name: "Nisarg Patel" },
+  { phone: "+91 8347160080" },
+  { email: "nisarg@pysquad.com" },
+  { location: "A 605, Shilp Aaron,Sindhu Bhavan Road, Ahmedabad-380054" },
+  { designation: "Odoo Expert, <br>Full-stack Developer" },
+  { website: "https://pysquad.com" },
+];
+
+export const profile = [
+  {
+    title: "Software Developer",
+    summary:
+      "Seeking to advance growing tech career as python developer. Offering skills in Backend development and object-oriented langauge to develope Business management like tools including CRM, e-commerce, billing, accounting, manufacturing, warehouse, project management, software application and other core systems.",
+  },
+];
+
+export const services = [
+  {
+    image: "assets/images/Crm.svg",
+    title: "Crm, Sales, Purchase",
+    description:
+      "Streamline customer relationships, sales pipelines, and procurement processes with tailored Odoo solutions.",
+  },
+
+  {
+    image: "assets/images/Accounting.svg",
+    title: "Accounting, Inventory",
+    description:
+      "Simplify financial management with automated accounting, invoicing, and reporting features.",
+  },
+
+  {
+    image: "assets/images/Website.svg",
+    title: "Website",
+    description:
+      "Build dynamic, user-friendly websites with Odoo’s integrated design and e-commerce tools.",
+  },
+
+  {
+    image: "assets/images/integration.svg",
+    title: "Integration, Migration, Customizations",
+    description:
+      "Seamlessly integrate, migrate, and customize Odoo to fit your unique business needs.",
+  },
+];
+
+export const education = [
+  {
+    degree: "MCA",
+    institute: "Kadi Sarva Vidhyalaya Gandhinagar",
+    year: "2016 - 2018",
+  },
+
+  // {
+  //   degree: "BCA",
+  //   institute: "HNGU",
+  //   year: "2012 - 2015",
+  // },
+];
+
+export const experience = [
+  {
+    designation: "Sr. Odoo Developer",
+    company: "PySquad Informatics",
+    year: "2024 - Present",
+  },
+
+  {
+    designation: "Sr. Odoo Developer",
+    company: "Pappaya Lite",
+    year: "2021 - 2024",
+  },
+
+  {
+    designation: "Python & Odoo Developer",
+    company: "PySquad Informatics",
+    year: "2020 - 2021",
+  },
+
+  {
+    designation: "Odoo Developer",
+    company: "Serpent Consulting Services Pvt. Ltd.",
+    year: "2018 - 2019",
+  },
+];
+
+export const skills = [
+  { name: "Python", progress: "75" },
+  { name: "Odoo Technical & Functional", progress: "90" },
+  { name: "Migration", progress: "90" },
+  { name: "Database Migration", progress: "80" },
+  { name: "Payment Gatway", progress: "70" },
+  { name: "Third Party Integration", progress: "85" },
+  { name: "PostgreSQL", progress: "70" },
+  { name: "MySQL", progress: "70" },
+  { name: "Git", progress: "80" },
+  { name: "Docker", progress: "50" },
+  { name: "Odoo.sh", progress: "80" },
+  { name: "PyTest", progress: "50" },
+  { name: "NumPy", progress: "50" },
+  { name: "Pandas", progress: "60" },
+  { name: "AWS / Digital Ocean / Google Cloud", progress: "70" },
+  { name: "Rest API", progress: "90" },
+  { name: "Tryton", progress: "60" },
+  { name: "Fast API", progress: "65" },
+  { name: "Web Scraping", progress: "50" },
+  { name: "JavaScript", progress: "60" },
+  { name: "Jquery", progress: "60" },
+  { name: "CSS", progress: "60" },
+];
+
+export const projects = [
+  {
+    name: "Gujarat FibreGrid (Govt. Project)",
+    image: "assets/images/projects/GFGNL.png",
+    page: "assets/images/projects/GFGNL.png",
+    description:
+      "Overseeing Gujarat's FiberGrid network, integrating billing and HRMS for efficiency. Ensures seamless connectivity, automates billing, HR functions, and network monitoring.",
+    alt: "Gujarat FibreGrid",
+    // technologies: "Python, Odoo-13.0, XML",
+    category: "", // Added category
+  },
+
+  {
+    name: "Gujarat council of Science City",
+    image: "assets/images/projects/Science_city.png",
+    page: "assets/images/projects/Science_city.png",
+    description:
+      "Gujarat Science City is a bold initiative of the Government of Gujarat to realize this priority. The Government is creating a sprawling center at Ahmedabad which aims to provide a perfect blend of education and entertainment. It will showcase contemporary and imaginative exhibits, minds on experiences, working models, virtual reality, activity corners, labs and live demonstrations to provide an understanding of science and technology to the common man.",
+    alt: "Gujarat council of Science City",
+    // technologies: "Python, Odoo-13.0, Js",
+    category: "", // Added category
+  },
+
+  {
+    name: "Herons Logistics",
+    image: "assets/images/projects/Herons.png",
+    page: "assets/images/projects/Herons.png",
+    description:
+      "Herons Logistics (I) Pvt Ltd was established in May 2011 by a group of Dynamic professionals having diverse in-depth knowledge and experience in Shipping and Logistics Industry. Within a short span of time HL has established itself as one of the operators to reckon within the Indian Trade.",
+    alt: "Herons Logistics",
+    // technologies: "Python, Odoo-13.0, Js",
+    category: "", // Added category
+  },
+
+  {
+    name: "Jeppiear University",
+    image: "assets/images/projects/Jeppier.png",
+    page: "assets/images/projects/Jeppier.png",
+    description:
+      "(School Management) In the ever-evolving landscape of education, institutions that aim to provide innovative, affordable, and globally relevant learning opportunities stand out. Jeppiaar University, established in 2022 in Chennai, India, is one such institution that aspires to provide cutting-edge education to students from around the world, all while keeping costs accessible. The university is not just a recent addition to the education sector but is an extension of the visionary journey initiated by the late Dr. Jeppiaar, through the Jeppiaar Educational Trust in 1987.",
+    alt: "Jeppiear University",
+    // technologies: "Python, Odoo-11.0, JSON",
+    category: "", // Added category
+  },
+
+  {
+    name: "Yokohama University",
+    image: "assets/images/projects/Yokohama.png",
+    page: "assets/images/projects/Yokohama.png",
+    description: "(Collage management)",
+    alt: "Yokohama University",
+    // technologies: "Python, Odoo-11.0",
+    category: "", // Added category
+  },
+
+  {
+    name: "DocVita One",
+    image: "assets/images/projects/Docvita.png",
+    page: "assets/images/projects/Docvita.png",
+    description: "Hospital Management",
+    alt: "DocVita One",
+    // technologies: "Python, Odoo-14.0, JavaScript, HTML, CSS",
+    category: "", // Added category
+  },
+];
